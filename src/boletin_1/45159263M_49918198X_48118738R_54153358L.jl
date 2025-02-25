b9ef73f6384d90f20c7ef0cf6924bbc97f2da681
fizge <fiz.garrido@udc.es>
2025-02-25 01:36:37 +0100

    importing optimisers

--- src/boletin_1/45159263M_49918198X_48118738R_54153358L.jl
+++ src/boletin_1/45159263M_49918198X_48118738R_54153358L.jl
@@ -3,6 +3,7 @@ using Statistics
 using Flux
 using Flux.Losses
 using Random
+using Optimisers
 
 ## One Hot Encoding
 
@@ -249,7 +250,7 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
     
     ann = buildClassANN(numTrainingInputs, topology, numTrainingOutputs; transferFunctions) # build ANN
     loss(model, x,y) = (size(y,1) == 1) ? Losses.binarycrossentropy(model(x),y) : Losses.crossentropy(model(x),y) # loss function
-    opt_state = Flux.setup(ADAM(learningRate), ann) # optimizer
+    opt_state = Flux.setup(Optimisers.ADAM(learningRate), ann) # optimizer
 
     trainingLosses = Float32[] # losses array
     validationLosses = Float32[]
@@ -330,3 +331,4 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
                          transferFunctions=transferFunctions, maxEpochs=maxEpochs, minLoss=minLoss, 
                          learningRate=learningRate, maxEpochsVal=maxEpochsVal)
 end;
+
