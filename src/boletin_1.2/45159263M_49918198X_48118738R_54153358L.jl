3991ee43f082ffaa2638d6070967567b84394c5d
fizge <fiz.garrido@udc.es>
2025-03-11 12:10:08 +0100

    first two "trainClassDoMe" functions

--- src/boletin_1.2/45159263M_49918198X_48118738R_54153358L.jl
+++ src/boletin_1.2/45159263M_49918198X_48118738R_54153358L.jl
@@ -31,3 +31,61 @@ function confusionMatrix(outputs::AbstractArray{<:Real,1},
     return confusionMatrix(outputs, targets)
 
 end
+
+#import Pkg; Pkg.add("SymDoME")
+
+using SymDoME
+
+# Función para clasificación binaria (targets es un vector booleano)
+function trainClassDoME(trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}},
+                        testInputs::AbstractArray{<:Real,2},
+                        maximumNodes::Int)
+    
+    (trainingInputs, trainingTargets) = trainingDataset
+
+    trainingInputs = Float64.(trainingInputs)
+    testInputs = Float64.(testInputs)
+
+    _, _, _, model = dome(trainingInputs, trainingTargets; maximumNodes = maximumNodes) 
+    println(string(model))
+    testOutputs = evaluateTree(model, testInputs)
+
+    if isa(testOutputs, Real)
+        testOutputs = repeat([testOutputs], size(testInputs, 1))
+    end
+
+    return testOutputs  
+end
+
+# Función para clasificación multiclase (targets es una matriz booleana)
+function trainClassDoME(trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}},
+                        testInputs::AbstractArray{<:Real,2},
+                        maximumNodes::Int)
+
+    (trainingInputs, trainingTargets) = trainingDataset
+
+    if size(trainingTargets, 2) == 1
+        # Si hay una sola columna, se trata como clasificación binaria.
+        testOutputs = trainClassDoME((trainingInputs, vec(trainingTargets)), testInputs, maximumNodes)
+        return reshape(testOutputs, :, 1)
+
+    elseif size(trainingTargets, 2) == 2
+        # En caso de dos columnas, se utiliza una de ellas (por ejemplo, la primera)
+        testOutputs = trainClassDoME((trainingInputs, vec(trainingTargets[:, 1])), testInputs, maximumNodes)
+        return reshape(testOutputs, :, 1)
+    
+    else 
+        # Estrategia "uno contra todos" para más de dos columnas.
+        num_instances = size(testInputs, 1)
+        num_classes = size(trainingTargets, 2)
+        testOutputsMatrix = Array{Float64}(undef, num_instances, num_classes)
+
+        for i in 1:num_classes
+            testOutputs = trainClassDoME((trainingInputs, vec(trainingTargets[:, i])), testInputs, maximumNodes)
+            testOutputsMatrix[:, i] = testOutputs
+        end 
+        return testOutputsMatrix
+    end
+end
+
+
